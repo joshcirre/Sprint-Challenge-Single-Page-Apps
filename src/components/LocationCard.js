17af563f49f16824ca4adac26d69b5b01c9e6e00
Josh Cirre <joshcirre@gmail.com>
2019-11-09 10:59:02 -0700

    added new image to homepage

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+const Style = styled.div`
+  max-width: 15rem;
+  width: 19%;
+  border: 1px solid black;
+  border-radius: 5px;
+  margin: 1rem;
+  img {
+    width: 100%;
+  }
+`;
+const Title = styled.div`
+  border-bottom: 1px solid black;
+  background-color: white;
+  padding: 1rem;
+  border-radius: 5px 5px 0 0;
+`;
+
+const Body = styled.div`
+  padding: 1rem;
+  background-color: rgb(255, 255, 255, 0.7);
+  border-radius: 0 0 5px 5px;
+`;
+
+export default function LocationCard({ location }) {
+  return (
+    <Style>
+      <Title>{location.name}</Title>
+      <Body>
+        {location.type + ' ' + location.dimension}
+        <br />
+        {' Number of Residents:' + location.residents.length}
+      </Body>
+    </Style>
+  );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+
+const Style = styled.div`
+  width: 100%;
+  img {
+    width: 100%;
+  }
+`;
 
 export default function WelcomePage() {
   return (
-    <section className="welcome-page">
-      <header>
-        <h1>Welcome to the ultimate fan site!</h1>
-        <img
-          className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="rick"
-        />
-      </header>
-    </section>
+    <Style>
+      <section className='welcome-page'>
+        <header>
+          <h1>Welcome to the ultimate fan site!</h1>
+          <img
+            className='main-img'
+            src='https://i.redd.it/ty3piks4k0zz.jpg'
+            alt='rick and morty image'
+          />
+        </header>
+      </section>
+    </Style>
   );
 }
