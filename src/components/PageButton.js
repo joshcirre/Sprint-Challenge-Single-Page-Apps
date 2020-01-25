4643509e0d21c2e98049e3883b9246509977cd54
Josh Cirre <hello@joshcirre.com>
2020-01-25 10:19:15 -0700

    Stretch Progress 💪

--- src/components/PageButton.js
+++ src/components/PageButton.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Button } from '@chakra-ui/core';
+import React from "react";
+import { Button } from "@chakra-ui/core";
 
 export default function({ direction, currentPage, setCurrentPage, maxpage }) {
   const handleClick = () => {
-    if (direction === 'previous') setCurrentPage(page => page - 1);
-    if (direction === 'next') setCurrentPage(page => page + 1);
+    if (direction === "previous") setCurrentPage(page => page - 1);
+    if (direction === "next") setCurrentPage(page => page + 1);
   };
 
-  const isPreviousDisabled = direction === 'previous' && currentPage <= 1;
-  const isNextDisabled = direction === 'next' && currentPage >= maxpage;
+  const isPreviousDisabled = direction === "previous" && currentPage <= 1;
+  const isNextDisabled = direction === "next" && currentPage >= maxpage;
   return (
     <Button
-      variantColor='teal'
-      variant='outline'
-      size='lg'
+      variantColor="teal"
+      variant="solid"
+      size="lg"
       onClick={handleClick}
       disabled={isNextDisabled || isPreviousDisabled}
     >
